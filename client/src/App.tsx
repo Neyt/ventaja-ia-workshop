7e4f76bcfd88d45ce8be67ada712b8036b5197fd
Ney Torres <Neyt@users.noreply.github.com>
2026-03-19 12:30:25 -0500

    Switch to useHashLocation for routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,14 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Router, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-const BASE = import.meta.env.BASE_URL.replace(/\/$/, "");
-
 function AppRouter() {
   return (
-    <Router base={BASE}>
+    <Router hook={useHashLocation}>
       <Switch>
         <Route path={"/"} component={Home} />
         <Route path={"/404"} component={NotFound} />
